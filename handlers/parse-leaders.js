d259c86e178bc793970e45dd30015697b8456fe4
Bryan Orozco <bryanorozcoweb@gmail.com>
2021-08-31 22:43:55 -0700

    dev: general updates

--- handlers/parse-leaders.js
+++ handlers/parse-leaders.js
@@ -6,6 +6,10 @@ exports.parseLeaders = (id, sheet)  => {
     return new Promise( async (resolve, reject) =>{
 
         const leagueLeaders = [];
+        const teamListCheck = [];
+        const teamList = [];
+        var teamID = 0;
+
         await fetch(`https://docs.google.com/spreadsheets/d/${id}/gviz/tq?tqx=out:csv&sheet=${sheet}`)
         .then(res => {
             res.body.pipe(csv({
@@ -19,16 +23,28 @@ exports.parseLeaders = (id, sheet)  => {
                 // Update percentage
                 _.forEach(data, function(value, key) {
                     if(value.indexOf("%") > -1) {
-                        const wholePercent = parseInt(value.replace(/%/g,""))
+                        const wholePercent = parseInt(value.replace(/%/g,""));
                         // data[key] = wholePercent/100
-                        data[`${key.replace(/%/g,"")}-percent`] = wholePercent/100
+                        data[`${key.replace(/%/g,"")}-percent`] = wholePercent/100;
                     }
                 });
+
+                if(!(teamListCheck.indexOf(data["team-name"]) > -1)) {
+                    teamListCheck.push(data["team-name"]);
+                    teamList.push({
+                        name: data["team-name"],
+                        id: teamID
+                    });
+                    teamID++;
+                }
 
                 leagueLeaders.push(data)
             })
             .on('close', async () => {
-                resolve(leagueLeaders);
+                resolve({
+                    "leagueLeaders":  leagueLeaders,
+                    "teamList": teamList
+                });
             });
         });
     })
