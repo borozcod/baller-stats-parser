db2b3e2829f26e4400e9ff4d221ac22f9f11a88a
Bryan Orozco <bryanorozcoweb@gmail.com>
2021-09-01 20:48:11 -0700

    dev: parser updates

--- handlers/parse-leaders.js
+++ handlers/parse-leaders.js
@@ -6,9 +6,6 @@ exports.parseLeaders = (id, sheet)  => {
     return new Promise( async (resolve, reject) =>{
 
         const leagueLeaders = [];
-        const teamListCheck = [];
-        const teamList = [];
-        var teamID = 0;
 
         await fetch(`https://docs.google.com/spreadsheets/d/${id}/gviz/tq?tqx=out:csv&sheet=${sheet}`)
         .then(res => {
@@ -29,22 +26,10 @@ exports.parseLeaders = (id, sheet)  => {
                     }
                 });
 
-                if(!(teamListCheck.indexOf(data["team-name"]) > -1)) {
-                    teamListCheck.push(data["team-name"]);
-                    teamList.push({
-                        name: data["team-name"],
-                        id: teamID
-                    });
-                    teamID++;
-                }
-
                 leagueLeaders.push(data)
             })
-            .on('close', async () => {
-                resolve({
-                    "leagueLeaders":  leagueLeaders,
-                    "teamList": teamList
-                });
+            .on('close', () => {
+                resolve(leagueLeaders);
             });
         });
     })
